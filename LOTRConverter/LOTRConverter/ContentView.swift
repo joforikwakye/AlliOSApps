1753746146bbdb42803985ec14325b4c3ba4a48c
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-05-08 00:13:47 +0000

    LOTRConverter Setup

---
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Jeffrey Kwakye on 07/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+                .padding()
+            Text("Hello, world!")
+                .foregroundColor(Color.blue)
+            Button("Button") {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Jeffrey Kwakye on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
