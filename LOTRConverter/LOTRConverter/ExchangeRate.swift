16cd965f71b65dc22b24a5952aaf921ce9e41ca3
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-05-12 23:42:09 +0000

    added a dismiss wrapper property

---
+++ LOTRConverter/LOTRConverter/ExchangeRate.swift
@@ -0,0 +1,8 @@
+//
+//  ExchangeRate.swift
+//  LOTRConverter
+//
+//  Created by Jeffrey Kwakye on 09/05/2024.
+//
+
+import Foundation
---
+++ LOTRConverter/LOTRConverter/SelectCurrency.swift
@@ -0,0 +1,18 @@
+//
+//  SelectCurrency.swift
+//  LOTRConverter
+//
+//  Created by Jeffrey Kwakye on 12/05/2024.
+//
+
+import SwiftUI
+
+struct SelectCurrency: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectCurrency()
+}
