b6b7599404d9c0ca34936097ba48b7c58d561a46
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-05-15 22:29:36 +0000

    the exchange rate screen

--- LOTRConverter/LOTRConverter/ExchangeRate.swift
+++ LOTRConverter/LOTRConverter/ExchangeRate.swift
@@ -5,4 +5,25 @@
 //  Created by Jeffrey Kwakye on 09/05/2024.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct ExchangeRate: View {
+    let leftImage: ImageResource
+    let text: String
+    let rightImage: ImageResource
+    
+    var body: some View {
+        HStack{
+            Image(leftImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 25)
+            Text(text)
+            Image(rightImage)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 25)
+        }
+    }
+}
