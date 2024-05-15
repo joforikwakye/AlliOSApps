000eea14d0424a4612fc002e4bd79397197cea79
Jeffrey Ofori Kwakye <jeffkwakye4u@gmail.com>
2024-05-15 22:28:09 +0000

    select currency screen with ontap

--- LOTRConverter/LOTRConverter/SelectCurrency.swift
+++ LOTRConverter/LOTRConverter/SelectCurrency.swift
@@ -8,11 +8,67 @@
 import SwiftUI
 
 struct SelectCurrency: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @State var selectedCurrency: Currency
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            VStack{
+                Text("Select the currency you are starting with:")
+                    .fontWeight(.bold)
+                
+                
+                
+                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                    
+                    ForEach(Currency.allCases) { currency in
+                        if selectedCurrency == currency {
+                            
+                            CurrencyIcon(currencyImage: currency.image, currencyText: currency.name)
+                                .shadow(color:.black, radius: 10)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .stroke(lineWidth: 3)
+                                        .opacity(0.5)
+                                }
+                        } else {
+                            CurrencyIcon(currencyImage: currency.image, currencyText: currency.name)
+                                .onTapGesture {
+                                    selectedCurrency = currency
+                                }
+                        }
+                        
+                    }
+                }
+                
+                Text("Select the currency you would like to convert to:")
+                    .fontWeight(.bold)
+                    
+                   
+
+                
+                Button("Done") {
+                    dismiss()
+                }
+                .tint(.brown)
+                .buttonStyle(.borderedProminent)
+                .padding()
+                .font(.title)
+                .foregroundStyle(.white)
+            }
+            .foregroundStyle(.black)
+            .padding()
+            .multilineTextAlignment(.center)
+        }
     }
 }
 
 #Preview {
-    SelectCurrency()
+    SelectCurrency(selectedCurrency: .goldPenny)
 }
+
